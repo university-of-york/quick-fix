8c1c35d293465b5b5df66c3c1aaaafcc04a9ab2b
Dave Hill <ThisDaveHill@users.noreply.github.com>
2020-04-29 20:23:52 +0100

    Update yorkutils.js

--- src/yorkutils.js
+++ src/yorkutils.js
@@ -252,7 +252,7 @@ $(document).ready(function(){
     function getYoutubeVideoTitle( videoID , callback ){
 
         // Authenticate access to API and retreive our video data
-        getURL( 'https://www.googleapis.com/youtube/v3/videos/?part=snippet&id='+videoID+'&key=AIzaSyAlfdQM8PF6qLxiEycc44I2eMdBoVIyyGA' , function( data )
+        getURL( 'https://www.googleapis.com/youtube/v3/videos/?part=snippet&id='+videoID+'&key=xxxx' , function( data )
         {
             data = JSON.parse( data );
 
